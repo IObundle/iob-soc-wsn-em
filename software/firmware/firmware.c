dffa7c8e30935507ae428a7742dc4f640212104a
Joao D Lopes <joao.d.lopes91@gmail.com>
2020-09-14 17:02:46 +0100

    continue to develop firmware; update ADPLL submodule

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -7,14 +7,11 @@
 #include "adpll.h"
 
 int main() {
-  // init uart
+  // Init uart
   uart_init(UART_BASE,FREQ/BAUD);   
   uart_printf("\n\n\nHello world!\n\n\n");
 
-  //char *a = malloc(10);
-  //free(a);
-
-  // init adpll
+  // Init adpll
   adpll_init(ADPLL_BASE);
 
   int adpll_mode = ADPLL_OPERATION;
@@ -46,4 +43,22 @@ int main() {
                FCW_mod,
                dco_c_l_word_test, dco_c_m_word_test, dco_c_s_word_test,
                dco_pd_test, tdc_pd_test, tdc_pd_inj_test);
+
+  adpll_soft_rst();
+
+  adpll_enable();
+
+  // Wait for results
+  unsigned int i;
+  unsigned int k = 0;
+  unsigned int end = SIM_TIME;
+  for (i = 0; i < end; i++) {
+    if (i == end*k/100) {
+      uart_printf("Loading progress: %d percent\n", k);
+      k += 10;
+    }
+  }
+  uart_printf("Loading progress: %d percent\n", 100);
+
+  return 0;
 }
