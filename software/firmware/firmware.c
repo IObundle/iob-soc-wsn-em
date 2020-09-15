e224790a0cfd9a0c9a1cd5f877f7490bd327084c
Joao D Lopes <joao.d.lopes91@gmail.com>
2020-09-16 00:34:46 +0100

    update ADPLL submodule; update simulation Makefile for ADPLL use (remove output files and use self-checker); update firmware according new driver implementation

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -7,18 +7,18 @@
 #include "adpll.h"
 
 int main() {
-  // Init uart
+  // Init UART
   uart_init(UART_BASE,FREQ/BAUD);   
   uart_printf("\n\n\nHello world!\n\n\n");
 
-  // Init adpll
+  // Init ADPLL
   adpll_init(ADPLL_BASE);
 
-  int adpll_mode = ADPLL_OPERATION;
+  // Configure ADPLL
+  int mode = ADPLL_OPERATION;
   int fcw = (int)(FREQ_CHANNEL*16384);
   uart_printf("freq_channel = %fMHz, FCW = %d, adpll_mode = %d\n", FREQ_CHANNEL, fcw, ADPLL_OPERATION);
 
-  // Configure ADPLL
   char alpha_l = 14;
   char alpha_m = 8;
   char alpha_s_rx = 7;
@@ -35,17 +35,20 @@ int main() {
   char dco_pd_test = 1;
   char tdc_pd_test = 1;
   char tdc_pd_inj_test = 1;
+  char tdc_ctr_freq = 0b100;
+  char dco_osc_gain = 0b10;
 
-  adpll_config(alpha_l, alpha_m, alpha_s_rx, alpha_s_tx,
+  adpll_config(fcw, mode,
+               alpha_l, alpha_m, alpha_s_rx, alpha_s_tx,
                beta,
                lambda_rx, lambda_tx,
                iir_n_rx, iir_n_tx,
                FCW_mod,
                dco_c_l_word_test, dco_c_m_word_test, dco_c_s_word_test,
-               dco_pd_test, tdc_pd_test, tdc_pd_inj_test);
+               dco_pd_test, dco_osc_gain,
+               tdc_pd_test, tdc_pd_inj_test, tdc_ctr_freq);
 
   adpll_soft_rst();
-
   adpll_enable();
 
   // Wait for results
