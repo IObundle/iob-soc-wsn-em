5ef0ea2975f42d511622ffe05a0444f606f09f17
Joao D Lopes <joao.d.lopes91@gmail.com>
2020-11-01 21:07:16 +0000

    update self-checker script invocation; update ADPLL submodule

--- hardware/simulation/simulation.mk
+++ hardware/simulation/simulation.mk
@@ -27,6 +27,7 @@ soc_tb.v:
 VSRC+=$(foreach p, $(PERIPHERALS), $(shell if test -f $(SUBMODULES_DIR)/$p/hardware/testbench/module_tb.sv; then echo $(SUBMODULES_DIR)/$p/hardware/testbench/module_tb.sv; fi;)) #add test cores to list of sources
 
 self-checker:
-	python3 $(ADPLL_PY_DIR)/self-checker.py 0 $(FREQ_CHANNEL)
+	python3 $(ADPLL_PY_DIR)/self-checker.py 0 $(FREQ_CHANNEL) SoC0
+	python3 $(ADPLL_PY_DIR)/self-checker.py 0 $(FREQ_CHANNEL)-1 SoC1
 
 .PHONY: run self-checker
