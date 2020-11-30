1d8f557c763395dd1288914f2f777d8008906a76
user4 <user4@micro7.lx.it.pt>
2020-11-30 19:17:20 +0000

    improve .tcl script

--- hardware/asic/umc130/synth/synscript.tcl
+++ hardware/asic/umc130/synth/synscript.tcl
@@ -1,12 +1,11 @@
-# library search paths
+#
+# library and hdl search paths
 set_attribute lib_search_path [list /opt/ic_tools/pdk/faraday/umc130/LL/fsc0l_d/2009Q2v3.0/GENERIC_CORE/FrontEnd/synopsys .]
-
 set libs [glob *.lib]
 set_attribute library [list fsc0l_d_generic_core_tt1p2v25c.lib $libs]
-
 set_attribute hdl_search_path $INCLUDE
-
-#verilog source files, defines and includes
+#
+# verilog source files, defines and includes
 echo "\n\n"
 echo "INCLUDE=" $INCLUDE
 echo "\n\n"
@@ -14,17 +13,33 @@ echo "DEFINE=" $DEFINE
 echo "\n\n"
 echo "VSRC=" $VSRC
 echo "\n\n"
-
-#verilog defines 
+#
+# verilog read
 read_hdl -v2001 -define $DEFINE $VSRC
+#
+# elaborate
 elaborate system
+#
+# constrains
 define_clock -name clk -period 31250 [find / -port clk] 
+#
+# synthesis
 synthesize -to_mapped
-#retime -prepare -min_delay
+#
+# aditional
+insert_tiehilo_cells -verbose
+delete_unloaded_undriven -all *
+#
+# reports
+report timing > timing_report.txt
+report power > power_report.txt
 report gates > gates_report.txt
 report area > area_report.txt
-report timing > timing_report.txt
-write_hdl -mapped > system_synth.v 
-write_sdc > system_synth.sdc
-
+#
+# outputs
+write_hdl -mapped -v2001 > system_synth.v 
+write_sdc -strict > system_synth.sdc
+write_db -to_file system_synth.sdc
+#
+#
 exit
