68cde72ec4897763ce8b19468f5b9370faa1c509
codingUniv <salisaf185@gmail.com>
2022-02-10 10:23:50 +0000

    start the development of the advertising procedure

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -1,125 +1,202 @@
+#include "iob-uart.h"
+#include "iob_timer.h"
+
 #include "id.h"
 #include "ble.h"
-
-#define N_BYTES 59   // Assumption: Max Payload length = 59
 
 int main() {
   unsigned long long elapsed;
   unsigned int elapsedu;
-
-  int i, d=1, sn_connect=0, bs_connect=0;
-  int pdu_size=0; // PDU size
-  char buffer[PDU_H_LEN+N_BYTES+CRC_LEN]; // PDU + CRC
-
+  uint32_t pdu_size_min=PDU_H_LEN;
+  uint32_t pdu_size_min_tmp=PDU_H_LEN+CRC_LEN;		   			
+  uint32_t pdu_size_max=PDU_H_LEN+MAX_PAYLOAD_LEN; 	// Max PDU size
+  char     *buffer_tx=NULL, *buffer_rx=NULL; 					
+  uint32_t j=0;					  
+  uint32_t start_time; 
+  
+  // By default, SN is in TX mode and ready to send a connectable advertising packet
+  sn_t sn = {.id = SN_ID, .aa_init = 0x8E89BED6, .nextState = MODE_SN_TX_ADV_IND};    	
+  
+  // By default, BS is in RX mode, free and can receive a connectable advertising packet
+  bs_t bs = {.id = BS_ID, .aa_init = 0x8E89BED6, .nextState = MODE_BS_RX_ADV_IND};
+  
+  pdu_adv_h_t pdu_adv_ind_h = {
+       .pdu_type=0,	      //4 bits
+       .rfu_1=0, 	      //2 bits
+       .tx_addr= 0,	      //1 bit
+       .rx_addr= 0,	      //1 bit
+       .length=4,	      //6 bits
+       .rfu_2=0  	      //2 bits
+  };
+   
   // Init ID
   id_init(ID_BASE);
-
-  // Init Timer
-  timer_init(TIMER_BASE);
-
-  // Init UART
-  uart_init(UART_BASE,FREQ/BAUD);
-  uart_printf("\n\n\nHello world!\n\n\n");
-
+  
   // Init BLE
   ble_init();
+  
+  // Init UART
+  uart_init(UART_BASE,FREQ/BAUD);
+  uart_printf("\n\n\nWelcome to WSN-EM!\n\n\n");
+  
+  while (j<2) { // will be replaced by while(1) to run with non-stop - the position of this loop might be changed 
+  
+  // Init Timer
+  timer_init(TIMER_BASE);
+  				
+  if(get_id()==sn.id) { // SN
+      switch(sn.nextState) {
+	case MODE_SN_TX_ADV_IND:	   	      
+	      // Configure ADPLL and BLE for the current data transmission	  
+  	      config_tx(FREQ_CHANNEL);   // Frequency to be changed	      
 
-  if(d==1) { // will be replaced by another condition
- 	sn_connect=1;   // SN Advertising - First time connection
-	bs_connect=1;   // BS Scanning - Connect with an SN for the first time
-	pdu_size=PDU_H_LEN+AA_LEN; 
-  } else { 
-  	sn_connect=0;	
-	bs_connect=0;
-  }
+  	      buffer_tx = (char*)malloc(pdu_size_min * sizeof(char));
+	      
+	      //Initialize buffer_tx
+	      for (int i = 0; i < pdu_size_min; i++) {
+	       	     buffer_tx[i] = 0;
+  	      }
+	      	      
+   	      // Build PDU	      	      
+	       buffer_tx[0] = (pdu_adv_ind_h.pdu_type << 4) | (pdu_adv_ind_h.rfu_1 << 2) | (pdu_adv_ind_h.tx_addr << 1) | pdu_adv_ind_h.rx_addr;
+	       buffer_tx[1] = (pdu_adv_ind_h.length << 2) | pdu_adv_ind_h.rfu_2; 
+	      
+	      // Write PDU to HW	
+  	      ble_send(buffer_tx, pdu_size_min);
 
-  // Assumptions
-  // ID(SN)=0 and ID(BS)=1  
-  // CRC is calculated over the PDU including PDU's header and PDU's payload 
-#if (MODE == RX)
-  if (get_id()) { // Sender
-#else // MODE == TX
-  if (!get_id()) { // Sender
-#endif
-    	switch(sn_connect) {
-		case 1: // SN Advertising - First time connection 	
-		    // Configure ADPLL
-		    ble_config(FREQ_CHANNEL, ADPLL_OPERATION);   // Frequency to be changed
+  	      // Wait for transmisstion
+  	      start_time = timer_time_us();
+  	      while ((timer_time_us() - start_time) < (unsigned int)1000);
 
-		    // Configure BLE for send data
-		    ble_send_on();
+  	      ble_off();
 
-		    // Build PDU = Header + Payload
-		    // PDU's Header
-		    buffer[0] = ADV_IND;
-		    buffer[1] = AA_LEN; 
-		    
-		    // PDU's Payload
-	            for (i = 2; i < pdu_size+2 ; i++) {  // will be replaced by AA
-	    	    	buffer[i] = 2*(i+1);
-		    }
+  	      // Print SN TX buffer for debugging purpose
+  	      uart_printf("\n%d - SN TX sent:\n", j);
+  	      for (int i = 0; i < pdu_size_min; i++) {
+  		  uart_printf("buffer[%d] = %x\n", i, buffer_tx[i]);
+  	      }	          	     
+	      
+	      free(buffer_tx);
+	      sn.nextState=MODE_SN_RX_CONNECT_REQ; bs.nextState=0;  	      	       	      
+  	      break;
+	      
+	case MODE_SN_RX_CONNECT_REQ:
+	      // Configure payload size, configure ADPLL and configure BLE for data reception
+	      config_rx(pdu_size_min, FREQ_CHANNEL);
 
-		    ble_send(buffer, pdu_size);
+  	      // Wait for transmission
+  	      start_time = timer_time_us();
+  	      while ((timer_time_us() - start_time) < (unsigned int)13000); 
 
-		    // Wait for transmisstion
-		    unsigned int start_time = timer_time_us();
-		    while ((timer_time_us() - start_time) < (unsigned int)1000);
+  	      // Receive data
+	      buffer_rx = (char*)malloc(pdu_size_min_tmp * sizeof(char));
+  	      for (int i = 0; i < pdu_size_min_tmp; i++) {
+	             buffer_rx[i] = 0;
+  	      }
+  	      char nbytes = ble_receive(buffer_rx, pdu_size_min_tmp);
 
-		    ble_off();
+  	      ble_off();
 
-		    // Print buffer
-		    uart_printf("\nsend:\n");
-		    for (i = 0; i < pdu_size; i++) {
-	            	uart_printf("buffer[%d] = %d\n", i, buffer[i]);
-		    }
-		    break;
-		default: printf("SN default"); // will be changed	    
-	}
-  } else { // Receiver
-    	switch(bs_connect) { 
-    		case 1: // BS Scanning - Connect with an SN for the first time
-		    // Payload
-    		    ble_payload(pdu_size);
+  	      // Wait for sender
+  	      start_time = timer_time_us();
+  	      while ((timer_time_us() - start_time) < (unsigned int)1000);
 
-    		    // Configure ADPLL
-       		    ble_config((FREQ_CHANNEL-1.0F), ADPLL_OPERATION);
+  	      // Print SN RX buffer for debugging purpose
+  	      uart_printf("\n%d - SN RX received %d bytes:\n", j, nbytes);
+  	      for (int i = 0; i < pdu_size_min_tmp; i++) {
+		  uart_printf("buffer[%d] = %x\n", i, buffer_rx[i]);
+  	      } 
+	      	      
+	      free(buffer_rx);
+	      sn.nextState=0; bs.nextState=0;	
+  	      break;	
+ 	
+	default: sn.nextState=0; bs.nextState=0;
+	         break;	     
+      }     
+    } else if (get_id()==bs.id) {  // BS
+     switch(bs.nextState) {
+        case MODE_BS_TX_CONNECT_REQ:	   
+	      // Configure ADPLL and BLE for data transmission	  
+  	      config_tx(FREQ_CHANNEL);   // Frequency to be changed	      	        
+	      
+	      buffer_tx = (char*)malloc(pdu_size_min * sizeof(char));
+  	      // Initialize buffer_tx 
+	      for (int i = 0; i < pdu_size_min; i++) {
+	             buffer_tx[i] = 0;
+  	      }
 
-    		    // Configure BLE for receive data
-    		    ble_recv_on();
+	      // Build PDU 
+  	      buffer_tx[0] = CONNECT_REQ; buffer_tx[1] = ZERO_PAYLOAD;   // ZERO_PAYLOAD used temporarily  
 
-    		    // Wait for transmission
-    		    unsigned int start_time = timer_time_us();
-    		    while ((timer_time_us() - start_time) < (unsigned int)10000);
+  	      ble_send(buffer_tx, pdu_size_min);
 
-    		    // Receive data
-    		    pdu_size += CRC_LEN;
-    		    for (i = 0; i < pdu_size; i++) {
-      			buffer[i] = 0;
-    		    }
-    		    char nbytes = ble_receive(buffer, pdu_size);
+  	      // Wait for transmisstion
+  	      start_time = timer_time_us();
+  	      while ((timer_time_us() - start_time) < (unsigned int)1000);
 
-    		    ble_off();
+  	      ble_off();
 
-    		    // Wait for sender
-    		    start_time = timer_time_us();
-    		    while ((timer_time_us() - start_time) < (unsigned int)1000);
+  	      // Print BS TX buffer for debugging purpose
+  	      uart_printf("\n%d - BS TX sent:\n", j);
+  	      for (int i = 0; i < pdu_size_min; i++) {
+  		  uart_printf("buffer[%d] = %x\n", i, buffer_tx[i]);
+  	      }
+  	      
+	      free(buffer_tx);
+	      bs.nextState=0; sn.nextState=0;
+	      break;
+	      
+  	case MODE_BS_RX_ADV_IND:  
+	      // Configure payload size, configure ADPLL and configure BLE for data reception
+	      config_rx(pdu_size_min, FREQ_CHANNEL);
 
-    		    // Print buffer
-    		    uart_printf("\nreceived %d bytes:\n", nbytes);
-    		    for (i = 0; i < pdu_size; i++) {
-      			uart_printf("buffer[%d] = %d\n", i, buffer[i]);
-    		    }
-		    break;
-		default: printf("BS default"); // will be changed    
-	}	
-  }
+  	      // Wait for transmission
+  	      start_time = timer_time_us();
+  	      while ((timer_time_us() - start_time) < (unsigned int)10000);
 
+  	      // Receive data
+	      buffer_rx = (char*)malloc(pdu_size_min_tmp * sizeof(char));
+  	      for (int i = 0; i < pdu_size_min_tmp; i++) {
+	      	     buffer_rx[i] = 0;
+  	      }
+  	      char nbytes = ble_receive(buffer_rx, pdu_size_min_tmp);
+
+  	      ble_off();
+
+  	      // Wait for sender
+  	      start_time = timer_time_us();
+  	      while ((timer_time_us() - start_time) < (unsigned int)1000);  
+
+  	      // Print BS RX buffer for debugging purpose
+  	      uart_printf("\n%d - BS RX received %d bytes:\n", j, nbytes);
+  	      for (int i = 0; i < pdu_size_min_tmp; i++) {
+		  uart_printf("buffer[%d] = %x\n", i, buffer_rx[i]);
+  	      } 
+	      
+	      if (nbytes) { // There is data in the rx buffer
+    	      	  if ((buffer_rx[0] >> 4)== 0) {
+			bs.nextState=MODE_BS_TX_CONNECT_REQ; sn.nextState=0; 
+              	  }
+	      }		      	       
+  	      free(buffer_rx);
+	      break;
+	      
+	 default: sn.nextState=0; bs.nextState=0;
+	          break;      
+     } 
+  }		  
+  
+  j++;
+  
   //read current timer count, compute elapsed time
   elapsed  = timer_get_count();
   elapsedu = timer_time_us();
 
   printf_("\nExecution time: %d clocks in %dus @%dMHz\n\n",
           (unsigned int)elapsed, elapsedu, FREQ/1000000);
-
+  
+  }
+  
   return 0;
 }
